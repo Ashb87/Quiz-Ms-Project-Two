a4d6e086167d217066927f11ea06f877b206b188
Ashb87 <ashleyb182@hotmail.co.uk>
2021-07-05 19:11:46 +0000

    added saving highscore function

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 //disables save score button if no name is input
@@ -9,6 +12,16 @@ username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = (event) => {
-    event.preventDefault();
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    console.log(highScores);
 };
